ff83f5103abdedc10356555583800dab60376bcd
Alena <alenzija@gmail.com>
2023-05-03 15:32:54 +0200

    feat: implement second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const fs = require('fs');
+const { stdout, stdin, exit } = process;
+
+stdout.write('Введите текст \n> ');
+stdin.on('data', (data) => {
+  fs.writeFile(path.join(__dirname, 'text.txt'), data.toString(), err => {
+    if (err) console.err(err.message);
+    console.log('Файл создан');
+    exit();
+  });
+})
