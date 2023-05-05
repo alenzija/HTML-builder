fa3e13f44c7d35a1eaa130dbcf1e0a72b996a7b2
Alena <alenzija@gmail.com>
2023-05-05 20:06:31 +0200

    feat: improve 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,29 +2,51 @@ const fs = require('fs');
 const path = require('path');
 
 const folderName = 'files';
-const copyFolderName = folderName + '-copy';
+const copyFolderName = folderName + '-copy'; 
 
-fs.mkdir(path.join(__dirname, copyFolderName), {recursive: true}, err => {
-  if (err) { 
-    console.error(err);
-  }
-  fs.readdir(path.join(__dirname, folderName), { withFileTypes: true }, (err, files) => {
-    if(err) throw err;
-    files.forEach(file => {
-      fs.copyFile(path.join(__dirname, folderName, file.name),path.join(__dirname,copyFolderName, file.name), err => {
-        if(err) console.log(err.message);
-      });
-    });
-    fs.readdir(path.join(__dirname, copyFolderName), { withFileTypes: true }, (err, copyfiles) =>{
-      if (err) console.error(err.message);
+const check = (files, pCopy) => {
+  fs.readdir(pCopy, { withFileTypes: true }, (err, copyfiles) =>{
+    if (err) {
+      console.error(err.message);
+    } else {
       files = files.map(file => file.name);
       copyfiles.forEach(file => {
-        if (files.indexOf(file.name) < 0) {
-          fs.unlink(path.join(__dirname, copyFolderName, file.name), err => {
+        if(file.isFile() && files.indexOf(file.name) < 0 ){      
+          fs.unlink(path.join(pCopy, file.name), err => {
             if (err) console.error(err);
           });
         }
       });
-    });
+    }
   });
+};
+
+const createCopy = (p, pCopy) => {
+  fs.readdir(p, { withFileTypes: true }, (err, files) => {
+    if(err) {
+      console.error(err.message);
+    } else {
+      files.forEach(file => {
+        if (file.isFile()) {
+          fs.copyFile(path.join(p, file.name), path.join(pCopy, file.name), err => {
+            if(err) console.log(err.message);
+          });
+        } else {
+          fs.mkdir(path.join(pCopy, file.name), {recursive: true}, err => {
+            if (err) { 
+              console.error(err);
+            } 
+            createCopy(path.join(p, file.name), path.join(pCopy, file.name));
+          });
+        }
+      });
+      check(files, pCopy);
+    }
+  });
+};
+
+fs.mkdir(path.join(__dirname, copyFolderName), {recursive: true}, err => {
+  if (err) { 
+    console.error(err);
+  } else createCopy(path.join(__dirname, folderName), path.join(__dirname, copyFolderName));
 });
