772e8dfbe3a96db0a3c28d01df3c62a6d1032870
Alena <alenzija@gmail.com>
2023-05-05 19:20:26 +0200

    feat: implement 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const path = require('path');
 
+const folderName = 'files';
+const copyFolderName = folderName + '-copy';
+
+fs.mkdir(path.join(__dirname, copyFolderName), {recursive: true}, err => {
+  if (err) { 
+    console.error(err);
+  }
+  fs.readdir(path.join(__dirname, folderName), { withFileTypes: true }, (err, files) => {
+    if(err) throw err;
+    files.forEach(file => {
+      fs.copyFile(path.join(__dirname, folderName, file.name),path.join(__dirname,copyFolderName, file.name), err => {
+        if(err) console.log(err.message);
+      });
+    });
+    fs.readdir(path.join(__dirname, copyFolderName), { withFileTypes: true }, (err, copyfiles) =>{
+      if (err) console.error(err.message);
+      files = files.map(file => file.name);
+      copyfiles.forEach(file => {
+        if (files.indexOf(file.name) < 0) {
+          fs.unlink(path.join(__dirname, copyFolderName, file.name), err => {
+            if (err) console.error(err);
+          });
+        }
+      });
+    });
+  });
+});
